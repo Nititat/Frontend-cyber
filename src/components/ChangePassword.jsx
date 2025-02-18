cfe7181e423d1f4f0d8eb4012934af068ec6b96e
Nititat <s6406022610023@email.kmutnb.ac.th>
2025-02-18 11:13:57 +0700

    up

--- src/components/ChangePassword.jsx
+++ src/components/ChangePassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "/css/changePassword.css";
+import "../components/css/changePassword.css";
 
 function ChangePassword() {
   
